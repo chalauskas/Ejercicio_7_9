409c06b75638ab06c7ed3fea16a0e00ccbc7bf4d
Gonzalo del Valle <chalauska@hotmail.com>
2020-11-13 08:52:15 +0100

    Modification of the exercise average.cs

--- average.cs
+++ average.cs
@@ -1,20 +1,32 @@
+// This program calculates the average of a set of marks, until the user types “end”.
+
 using System;
-public class Average{
-public static void Main(){
-double a=0, b, c=0, d;
-string s;
-Console.Write("Insert a mark: ");
-s=Console.ReadLine();	// we read the mark as string to check the word "end"	 
-while(s!="end"){  // if the mark isn't an end then we pass it to double to work with it
-b=Convert.ToDouble(s);
-c++;   //counter of numbers
-a+=b;  // sum of all the notes
-Console.Write("Insert a mark: ");
-s=Console.ReadLine();
-}
-if(c!=0){
-	d=a/c;  //calculate the average
-Console.WriteLine("The average is {0}.",d);
-}
-}
+
+public class Average
+{
+	public static void Main()
+	{
+		double sumOfMarks=0, marksInNumber, numberOfImputs=0, averageMark;
+		string finishProgram;
+
+		Console.Write("Insert a mark: ");
+		finishProgram = Console.ReadLine();		
+		
+		while(finishProgram != "end")
+		{  
+			marksInNumber=Convert.ToDouble();
+
+			numberOfImputs++;   
+			sumOfMarks += marksInNumber;  
+
+			Console.Write("Insert a mark: ");
+			finishProgram=Console.ReadLine();
+		}
+		
+		if(numberOfImputs != 0)
+		{
+			averageMark=a/numberOfImputs; 
+			Console.WriteLine("The average is {0}.",averageMark);
+		}
+	}
 }
